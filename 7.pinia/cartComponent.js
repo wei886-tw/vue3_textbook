386ba2e4e32be8d09b939dd25d3658666584d85f
wei886-tw <lunhowl@gmail.com>
2024-02-12 18:50:02 +0800

    no render picture

--- 7.pinia/cartComponent.js
+++ 7.pinia/cartComponent.js
@@ -7,25 +7,25 @@ export default {
     ...mapState(cartStore, ["cartList"])
   },
   template: `<div class="bg-light p-4 my-4">
-  <div v-if="cartList.cartItem.length === 0">購物車沒有任何品項</div>
-  <table class="table align-middle" v-else>
+
+  <table class="table align-middle">
     <tbody>
-      <tr v-for="cart in cartList" :key="cart.id">
+      <tr v-for="cart in cartList.cartItem" :key="cart.id">
         <td width="100">
           <a href="#" class="text-dark"><i class="fas fa-times"></i></a>
         </td>
         <td width="100">
           <img
-            src="https://images.unsplash.com/photo-1597733153203-a54d0fbc47de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1090&q=80"
+            :src="cart.product.imageUrl"
             class="table-image img-fluid" alt="" >
         </td>
-        <td>爽快快的</td>
+        <td>{{cart.product.title}}</td>
         <td width="200">
           <select name="" id="" class="form-select">
             <option value="1">1</option>
           </select>
         </td>
-        <td width="200" class="text-end"></td>
+        <td width="200" class="text-end">{{cart.product.subTotal}}</td>
       </tr>
     </tbody>
     <tfoot>
